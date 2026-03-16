5b2fe86262048e314771bd475162c8e940417c6d
Harsh Lalwala <hlalwala4950gmail.com>
2026-03-16 13:31:49 +0530

    Update portfolio

--- src/component/myskills/skill.js
+++ src/component/myskills/skill.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './skill.css'
+import react from "../../assets/images/icons/react.png";
 import html5 from "../../assets/images/icons/html5.png";
 import css3 from "../../assets/images/icons/css3.png";
 import js from "../../assets/images/icons/js.png";
@@ -13,52 +14,53 @@ export default function skill() {
     const skills = [
         {
             id: 1,
+            title: "React",
+            SkillImage: react,
+            alt: "React",
+        },
+        {
+            id: 2,
             title: "HTML 5",
             SkillImage: html5,
             alt: "HTML 5",
         },
         {
-            id: 2,
+            id: 3,
             title: "CSS 3",
             SkillImage: css3,
             alt: "CSS 3",
         },
         {
-            id: 3,
+            id: 4,
             title: "JavaScript",
             SkillImage: js,
             alt: "JavaScript",
         },
+        
         {
-            id: 4,
-            title: "jQuery",
-            SkillImage: jquery,
-            alt: "jQuery",
-        },
-        {
-            id: 5,
+            id: 6,
             title: "Bootstrap",
             SkillImage: bootstrap,
             alt: "Bootstrap",
         },
         {
-            id: 6,
+            id: 7,
             title: "ASP.NET",
             SkillImage: aspnet,
             alt: "ASP.NET",
         },
         {
-            id: 7,
+            id: 8,
             title: "Basic SEO",
             SkillImage: seo,
             alt: "Basic SEO",
         },
         {
-            id: 8,
+            id: 9,
             title: "Google Analytics",
             SkillImage: googleanalytics,
             alt: "Google Analytics",
-        },
+        }
     ];
 
     return (
